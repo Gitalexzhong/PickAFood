2e0ff9a7fbb5204d2193632dbdc2f2381eaf8e07
Gitalexzhong <66319418+Gitalexzhong@users.noreply.github.com>
2022-06-12 23:08:49 +1000

    Added some return params

--- sessions.py
+++ sessions.py
@@ -1,20 +1,20 @@
 # Python modules for interacting with sessions 
 
-def start_session():
+def create_session(uid: int) -> int:
     pass
 
-def end_session():
+def end_session(id: int, uid: int) -> bool:
     pass
 
-def join_session():
+def start_session(id: int, uid: int) -> dict:
     pass
 
-def start_session():
+def join_session(id: int, uid: int) -> bool:
     pass
 
-def session_exists():
+def session_exists(id: int) -> bool:
     pass
 
-def session_stats():
+def session_stats(id: int) -> dict:
     pass
 
