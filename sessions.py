6c58ed9bdee9a2d05c5aa70f8ab4bb7a791307a0
Gitalexzhong <66319418+Gitalexzhong@users.noreply.github.com>
2022-06-12 23:01:58 +1000

    Added Stubs for session session modules

---
+++ sessions.py
@@ -0,0 +1,15 @@
+# Python modules for interacting with sessions 
+
+def start_session():
+    pass
+
+def end_session():
+    pass
+
+def join_session():
+    pass
+
+def start_session():
+    pass
+
+
