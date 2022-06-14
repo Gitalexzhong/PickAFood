498f7dfbca083b2fc08f4f05ebb1f1c3c7d096e9
Gitalexzhong <66319418+Gitalexzhong@users.noreply.github.com>
2022-06-15 01:18:58 +1000

    added button

---
+++ frontend/src/components/toggle-button.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+import { Button } from 'react-native'
+const Test = () => {
+    return(
+<button onClick={sayHello}>Default</button>;
+    )
+}
+export default Test
