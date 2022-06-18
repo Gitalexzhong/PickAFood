4e8e84a447872410b9b2155701869ea7cc1c2558
Gitalexzhong <66319418+Gitalexzhong@users.noreply.github.com>
2022-06-18 23:25:15 +1000

    Added working button

--- frontend/src/components/toggle-button.js
+++ frontend/src/components/toggle-button.js
@@ -3,10 +3,32 @@ import '../App.css';
 // import React, { Component }  from 'react';
 import React from 'react'
 
+function sayHello() {
+  alert('You clicked me!');
+}
+
 function Yes() {
+  // return (
+  //   <div className="App">
+  //     <header>
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
   return (
-    <div className="App">
-      <header>
+  <div className="App">
+      {/* <header>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -19,9 +41,10 @@ function Yes() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <button onClick={sayHello}>Default</button>;
     </div>
-  );
+  )
 }
 
 export default Yes;
