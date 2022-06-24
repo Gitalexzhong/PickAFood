eb07966e17641ee99f0c305b2944085c53d07b01
Gitalexzhong <66319418+Gitalexzhong@users.noreply.github.com>
2022-06-24 23:26:39 +1000

    Chnaged to different toggle switch

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Yes from './components/toggle-button'
+// import ToggleSwitch from './components/toggle-button'
+import ToggleSwitch from "./components/ToggleSwitch";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <Yes/>
+    <ToggleSwitch label="Notifications" />
   </React.StrictMode>
 );
 
