166f06f3adbcf3736952404db99dfe08e088cf84
Gitalexzhong <66319418+Gitalexzhong@users.noreply.github.com>
2022-07-02 20:41:37 +1000

    got passing text to work correctly

--- frontend/src/Elements/ToggleButtonOnOff.js
+++ frontend/src/Elements/ToggleButtonOnOff.js
@@ -8,28 +8,41 @@ import theme from '../Themes/MainTheme'
 // import Container from '@mui/material/Container';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import ToggleButton from '@mui/material/ToggleButton';
+import Switch from '@mui/material/Switch';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
 
 // function ToggleState() {
 //   alert("This is an alert"); 
 // }
+const label = { inputProps: { 'aria-label': 'Switch demo' } };
 
-function ToggleButtonOnOff({textName}) {
-  const [alignment, setAlignment] = React.useState('web');
+function ToggleButtonOnOff({ textName }) {
+  // const [alignment, setAlignment] = React.useState('web');
 
-  const handleChange = (event, newAlignment) => {
-    setAlignment(newAlignment);
-  };
+  // const handleChange = (event, newAlignment) => {
+  //   setAlignment(newAlignment);
+  // };
+
+  // return (
+  //   <ToggleButtonGroup
+  //     color="secondary"
+  //     value={alignment}
+  //     exclusive
+  //     onChange={handleChange}
+  //   >
+  //     <ToggleButton value='false'>{textName}</ToggleButton>
+  //   </ToggleButtonGroup>
+  // );
 
   return (
-    <ToggleButtonGroup
-      color="secondary"
-      value={alignment}
-      exclusive
-      onChange={handleChange}
-    >
-      <ToggleButton value='false'>{textName}</ToggleButton>
-    </ToggleButtonGroup>
-  );
+    <div>
+      <FormGroup>
+        <FormControlLabel control={<Switch defaultChecked />} label={textName} />
+        <FormControlLabel disabled control={<Switch />} label="Disabled" />
+      </FormGroup>
+    </div>
+  )
 }
 
 // ReactDOM.render(
