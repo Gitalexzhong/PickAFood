cee5805574974a25d142687633f6b1eeb0d5ca62
Gitalexzhong <66319418+Gitalexzhong@users.noreply.github.com>
2022-07-03 21:52:09 +1000

    Setup for mutliple text

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <ToggleButtonOnOff textName='passed text'/>
+    <ToggleButtonOnOff textName='passed text multiple'/>
   </React.StrictMode>
 );
 
